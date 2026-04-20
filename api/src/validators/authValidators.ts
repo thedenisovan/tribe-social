2d1123051e02f1896548ad16c1b29856457b37d5
Dainis Dilevka <thedenisovan@proton.me>
2026-04-20 19:00:09 +0100

    add sign in validator

--- api/src/validators/authValidators.ts
+++ api/src/validators/authValidators.ts
@@ -1,28 +1,29 @@
 import { body } from 'express-validator';
 import prismaNeon from '../db/prisma.js';
-
-enum Name {
-  FirstName,
-  LastName,
-}
+import bcrypt from 'bcryptjs';
 
 const alphaErr = 'must only contain letters.';
+const generalErr = 'Invalid username or password.';
 const isLengthErr = 'must be between 3 and 16 characters.';
 const notEmptyErr = 'is required.';
 
-// Validator chain
+// Signup validator chain
 const signupValidator = [
-  nameValidator(Name.FirstName),
-  nameValidator(Name.LastName),
-  emailValidator(),
+  nameValidator(true),
+  nameValidator(false),
+  emailValidator(true),
   passwordValidator(),
+  passwordConfirmationValidator(),
 ];
 
-// Based on function input return validator for firstName or lastName from form body
-function nameValidator(name: Name) {
-  const currentName = name === Name.FirstName ? 'First name' : 'Last name';
+// Signin validator chain
+const signinValidator = [emailValidator(false), ...signinPasswordValidator()];
 
-  return body(name === Name.FirstName ? 'firstName' : 'lastName')
+// Based on function input return validator for firstName or lastName from form body
+function nameValidator(isFirstName: boolean) {
+  const currentName = isFirstName ? 'First name' : 'Last name';
+
+  return body(isFirstName ? 'firstName' : 'lastName')
     .trim()
     .notEmpty()
     .withMessage(`${currentName} ${notEmptyErr}`)
@@ -34,7 +35,7 @@ function nameValidator(name: Name) {
     .withMessage(`${currentName} ${isLengthErr}`);
 }
 
-function emailValidator() {
+function emailValidator(isSignupValidator: boolean) {
   return body('email')
     .trim()
     .notEmpty()
@@ -47,31 +48,73 @@ function emailValidator() {
         where: { email: value },
       });
 
-      if (user) throw new Error(`E-mail already in use.`);
+      // If user exists and this is signupValidator
+      if (user && isSignupValidator) {
+        throw new Error(`E-mail already in use.`);
+        // Else if this is signinValidator and no user found throw this error
+      } else if (!user && !isSignupValidator) {
+        throw new Error(generalErr);
+      }
+
+      return true;
     });
 }
 
 function passwordValidator() {
-  return (
-    body('password')
-      .trim()
-      .notEmpty()
-      .withMessage(`Password ${notEmptyErr}`)
-      .bail()
-      .matches(/^(?=.*[A-Z]).{6,}$/)
-      .withMessage(
-        'Password must be 6+ chars with at least one uppercase letter.',
-      ),
-    body('passwordConfirmation')
-      .trim()
-      .notEmpty()
-      .withMessage(`Password confirmation ${notEmptyErr}`)
-      .bail()
-      .custom(async (value, { req }) => {
-        if (value !== req.body.password)
-          throw new Error('Passwords did not match.');
-      })
-  );
+  return body('password')
+    .trim()
+    .notEmpty()
+    .withMessage(`Password ${notEmptyErr}`)
+    .bail()
+    .matches(/^(?=.*[A-Z]).{6,}$/)
+    .withMessage(
+      'Password must be 6+ chars with at least one uppercase letter.',
+    );
 }
 
-export { signupValidator };
+function passwordConfirmationValidator() {
+  return body('passwordConfirmation')
+    .trim()
+    .notEmpty()
+    .withMessage(`Password confirmation ${notEmptyErr}`)
+    .bail()
+    .custom(async (value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords did not match.');
+      }
+
+      return true;
+    });
+}
+
+// Validator to make sure that user enters correct password to given email
+function signinPasswordValidator() {
+  // Return array of two validators, later will flatten in in main signin validator array
+  return [
+    passwordValidator(),
+    body('password').custom(async (value, { req }) => {
+      const user = await prismaNeon.user.findUnique({
+        where: { email: req.body.email },
+      });
+
+      // If in case user is not found throw general error
+      if (!user) {
+        throw new Error(generalErr);
+      }
+
+      const passComparisonResult = await bcrypt.compare(
+        value,
+        user?.hashedPassword,
+      );
+
+      // If password does not match
+      if (!passComparisonResult) {
+        throw new Error(generalErr);
+      }
+
+      return true;
+    }),
+  ];
+}
+
+export { signupValidator, signinValidator };
