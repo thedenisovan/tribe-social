0222aca8b8efdf9cc74697d1b662804d5fa5484e
Dainis Dilevka <thedenisovan@proton.me>
2026-05-08 16:03:01 +0100

    update: mw to include full user post/saved post data in query of getting full user and removed mw of getting user posts seperet

--- api/src/controllers/get/getUserData.ts
+++ api/src/controllers/get/getUserData.ts
@@ -2,41 +2,6 @@ import prismaNeon from '../../db/prisma.js';
 import { Request, Response, NextFunction } from 'express';
 import { HttpError } from '../../middleware/errorMiddleware.js';
 
-export default async function getUserPosts(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  try {
-    const { authorId } = req.params;
-
-    if (!authorId) {
-      return next(new HttpError(`No author id provided.`, 400));
-    }
-
-    const authorIntId = Number(authorId);
-
-    if (!authorIntId || isNaN(authorIntId)) {
-      return next(
-        new HttpError(
-          `No or incorrect values were provided for request body.`,
-          400,
-        ),
-      );
-    }
-
-    // Get all posts created by user
-    const posts = await prismaNeon.post.findMany({
-      where: { authorId: authorIntId },
-      include: { likes: true, saved: true, comments: true, hashtags: true },
-    });
-
-    return res.status(201).json(posts);
-  } catch (e) {
-    next(e);
-  }
-}
-
 export async function getPost(req: Request, res: Response, next: NextFunction) {
   try {
     const { postId } = req.params;
@@ -95,8 +60,26 @@ export async function getUserProfileData(
     const userData = await prismaNeon.user.findUnique({
       where: { id: intUserId },
       include: {
-        posts: true,
-        savedPosts: true,
+        posts: {
+          include: {
+            likes: true,
+            saved: true,
+            comments: true,
+            hashtags: true,
+          },
+        },
+        savedPosts: {
+          include: {
+            post: {
+              include: {
+                likes: true,
+                saved: true,
+                comments: true,
+                hashtags: true,
+              },
+            },
+          },
+        },
         follower: true,
         following: true,
         requester: true,
--- api/src/routes/profile.route.ts
+++ api/src/routes/profile.route.ts
@@ -1,15 +1,12 @@
 import { Router } from 'express';
 import { verifyToken } from '../middleware/jwt.js';
-import getUserPosts, {
-  getUserProfileData,
-} from '../controllers/get/getUserData.js';
+import { getUserProfileData } from '../controllers/get/getUserData.js';
 import validResult from '../validators/validationResult.js';
 import { updatePersonalDataValidator } from '../validators/authValidators.js';
 import updateUserDetails from '../controllers/put/updateUserProfile.js';
 
 const profileRoute = Router();
 
-profileRoute.get('/getUserPosts/:authorId', verifyToken, getUserPosts);
 profileRoute.get('/getUserProfile/:userId', verifyToken, getUserProfileData);
 
 profileRoute.put(
